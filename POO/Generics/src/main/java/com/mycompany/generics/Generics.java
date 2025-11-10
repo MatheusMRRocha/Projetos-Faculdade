aab437bf0c190b3ef0fdad55faf2104575d921a3
MatheusMRRocha <matheusmreisrocha@gmail.com>
2025-11-10 03:27:58 -0300

    Finalização 1
    Possivel finalização, ainda se tem a nescessidade de checar todos os requisistos

--- POO/Generics/src/main/java/com/mycompany/generics/Generics.java
+++ POO/Generics/src/main/java/com/mycompany/generics/Generics.java
@@ -17,6 +17,29 @@ public class Generics {
 
 // Main.java (Demonstração do problema)
     public static void main(String[] args) {
+        Deposito<Eletronico> depositoEletronico = new Deposito<>();
+        Deposito<Alimento> depositoAlimento = new Deposito<>();
+        
+        depositoEletronico.adicionarItem(new Eletronico("Smartphone"));
+        depositoEletronico.adicionarItem(new Eletronico("Notebook"));
+        
+        //Amazenamento com erro para apresentação
+        //depositoEletronico.adicionarItem(new Alimento("Maçã"));
+        //Amazenamento correto
+        depositoAlimento.adicionarItem(new Alimento("Maca"));
+        
+        Eletronico primeiroItemE = depositoEletronico.obterItem(0);
+        System.out.println("Item recuperado: " + primeiroItemE);
+
+        Eletronico segundoItemE = depositoEletronico.obterItem(1);
+        System.out.println("Item recuperado: " + segundoItemE);
+        
+        
+        Alimento primeiroItemA = depositoAlimento.obterItem(0);
+        System.out.println("Item recuperado: "+ primeiroItemA);
+        
+        /*Utilizado para teste de um lista que aceita qualquer classe
+        
         Deposito<Produto> depositoGeral = new Deposito<>();
 
         depositoGeral.adicionarItem(new Eletronico("Smartphone"));
@@ -27,6 +50,6 @@ public class Generics {
         System.out.println("Item recuperado: " + primeiroItem);
 
         Produto itemProblematico = depositoGeral.obterItem(2);
-        System.out.println("Item recuperado: " + itemProblematico);
+        System.out.println("Item recuperado: " + itemProblematico);*/
     }
 }
